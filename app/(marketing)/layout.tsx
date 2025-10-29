822a776d0f1ebba08092e5230db8c738951e670b
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-10-29 10:12:46 +0530

    Changes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -8,8 +8,9 @@ export default function MarketingLayout({
 }) {
     return (
         <main className="w-full grow relative">
+                <div className="absolute -top-16 inset-x-0 -z-10 mx-auto w-3/4 h-32 lg:h-40 rounded-full blur-[5rem] bg-[radial-gradient(86.02%_172.05%_at_50%_-40%,rgba(18,139,135,1)_0%,rgba(5,5,5,0)_80%)]"></div>
             <Navbar />
-            {children}
+            {children}  
         </main>
     );
 };
