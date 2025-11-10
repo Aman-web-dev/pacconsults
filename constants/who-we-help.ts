4f2e4bc32e1767ed6dafa58f9238b36c4cb928dd
Siddhant Gupta <71984091+sid346184@users.noreply.github.com>
2025-11-11 04:26:28 +0530

    Update who-we-help.ts

--- constants/who-we-help.ts
+++ constants/who-we-help.ts
@@ -1,21 +1,21 @@
 export const WHO_WE_HELP = [
     {
         title: "Business Owners Ready to Sell",
-        description: "Business Lines of Credit – Draw funds as needed to manage cash flow or expand operations",
+        description: "Unlock maximum value for your business with tailored funding solutions and expert exit guidance — designed to make your sale smooth, fast, and profitable.",
         image: "/images/seller.png",
         cta: "CONTACT US",
         ctaLink: "https://calendly.com/pacconsulting/investororsellerdiscoverycall?back=1"
     },
     {
         title: "Operators Seeking Growth Capital",
-        description: "0% Interest Business Credit Cards – Perfect for startups and short-term working capital.",
+        description: "Scale faster with flexible growth capital — from 0% interest credit lines to strategic funding plans that fuel expansion without draining your cash flow.",
         image: "/images/creditlender.png",
         cta: "CONTACT US",
         ctaLink: "https://calendly.com/pacconsulting/personal-business-funding?back=1"
     },
     {
-        title: "CPAs/Attorneys/Bankers with Clients to Refer",
-        description: "Revenue-Based Lending – Fast funding based on your company’s monthly sales performance.",
+        title: "CPAs, Attorneys & Bankers with Clients to Refer",
+        description: "Help your clients access fast, performance-based funding while you strengthen relationships and add value — partner with us for seamless, trusted referrals.",
         image: "/images/attorney.png",
         cta: "CONTACT US",
         ctaLink: "https://calendly.com/pacconsulting/30-minute-consultation-clone?back=1"
